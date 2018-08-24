1a2e98bc5b65e1d06ccc96dfbe491fda3135b7c5
XxFri3ndlyxX <loll420@live.ca>
2018-08-24 00:49:18 -0400

    Update and rename __resource to __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -7,7 +7,6 @@ client_scripts {
 	'@es_extended/locale.lua',
 	'client/main.lua',
 	'locales/en.lua',
-	'locales/sv.lua',
 	'config.lua'
 }
 
@@ -15,6 +14,5 @@ server_scripts {
 	'@es_extended/locale.lua',
 	'server/main.lua',
 	'locales/en.lua',
-	'locales/sv.lua',
 	'config.lua'
 }
