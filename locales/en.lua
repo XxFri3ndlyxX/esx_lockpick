a4e00c5023ae24f09eedf45acf8d081a5b6c80a1
XxFri3ndlyxX <loll420@live.ca>
2018-08-24 00:48:19 -0400

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,8 +1,8 @@
 Locales ['en'] = {
-	['used_kit']					= 'you used ~y~x1 ~b~lockpick',
-	['must_be_outside']				= 'you must be outside of the vehicle!',
-	['no_vehicle_nearby']			        = 'there is no ~r~vehicle ~w~nearby',
-	['vehicle_unlocked']				= '~g~you lockpicked the vehicle!',
-	['abort_hint']					= 'press ~INPUT_VEH_DUCK~ to cancel',
-	['aborted_lockpicking']				= 'you ~r~aborted ~w~the lockpicking',
+	['used_kit']					= 'You used x1 lockpick',
+	['must_be_outside']				= 'You must be outside of the vehicle!',
+	['no_vehicle_nearby']			= 'There is no vehicle nearby',
+	['vehicle_unlocked']			= 'You lockpicked the vehicle!',
+	['abort_hint']					= 'Press ~INPUT_VEH_DUCK~ to cancel',
+	['aborted_lockpicking']		    = 'You aborted the lockpicking',
 }
