d05d143b6be9510030d6df20d2fb5748cb8d4eb3
XxFri3ndlyxX <loll420@live.ca>
2018-08-24 00:49:47 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config						= {}
-Config.InfiniteRepairs		= false -- Should one lockpick last forever?
-Config.RepairTime			= 15 -- In seconds, how long should lockpicking take?
-Config.IgnoreAbort			= true -- Remove lockpick from inventory even if user aborts lockpicking?
-Config.AllowMecano			= false -- Allow mechanics to use this lockpick?
+Config.InfiniteLocks		= false  -- Should one lockpick last forever?
+Config.LockTime			    = 45     -- In seconds, how long should lockpicking take?
+Config.IgnoreAbort			= true   -- Remove lockpick from inventory even if user aborts lockpicking?
+Config.AllowMecano			= false  -- Allow mechanics to use this lockpick?
 
 Config.Locale				= 'en'
