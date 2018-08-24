94cd96ce2e1c8bed28a66ddd231a1a0427827b40
XxFri3ndlyxX <loll420@live.ca>
2018-08-23 21:56:54 -0400

    Create main.lua

---
+++ server/main.lua
@@ -0,0 +1,27 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+-- Make the kit usable!
+ESX.RegisterUsableItem('lockpick', function(source)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	if Config.AllowMecano then
+		TriggerClientEvent('esx_lockpick:onUse', _source)
+	else
+		if xPlayer.job.name ~= 'mecano' then
+			TriggerClientEvent('esx_lockpick:onUse', _source)
+		end
+	end
+end)
+
+RegisterNetEvent('esx_lockpick:removeKit')
+AddEventHandler('esx_lockpick:removeKit', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	if not Config.InfiniteRepairs then
+		xPlayer.removeInventoryItem('lockpick', 1)
+		TriggerClientEvent('esx:showNotification', _source, _U('used_kit'))
+	end
+end)
