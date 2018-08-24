6ea49688210301dafb896f363d8b3300b1490593
XxFri3ndlyxX <loll420@live.ca>
2018-08-23 22:03:22 -0400

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,8 +1,8 @@
 Locales ['en'] = {
 	['used_kit']					= 'you used ~y~x1 ~b~lockpick',
 	['must_be_outside']				= 'you must be outside of the vehicle!',
-	['no_vehicle_nearby']			= 'there is no ~r~vehicle ~w~nearby',
-	['finished_repair']				= '~g~you lockpicked the vehicle!',
+	['no_vehicle_nearby']			        = 'there is no ~r~vehicle ~w~nearby',
+	['vehicle_unlocked']				= '~g~you lockpicked the vehicle!',
 	['abort_hint']					= 'press ~INPUT_VEH_DUCK~ to cancel',
-	['aborted_repair']				= 'you ~r~aborted ~w~the lockpicking',
+	['aborted_lockpicking']				= 'you ~r~aborted ~w~the lockpicking',
 }
