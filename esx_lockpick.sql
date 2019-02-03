5cc20c88bd37aa9d73009f7505499a619ec856e4
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-02-03 05:20:47 -0500

    Update esx_lockpick.sql

--- esx_lockpick.sql
+++ esx_lockpick.sql
@@ -1,2 +1,11 @@
-INSERT INTO items (`name`,`label`,`limit`,`rare`,`can_remove`) VALUES
-('lockpick', 'Lockpick', 3, 0, 1);
+USE `essentialmode`;
+
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('lockpick', 'Lockpick', 3, 0, 1)
+;
+INSERT INTO `shops` (`store`, `item`, `price`) VALUES
+('TwentyFourSeven', 'lockpick', 10000),
+('LTDgasoline', 'lockpick', 10000),
+('RobsLiquor', 'lockpick', 10000)
+;
