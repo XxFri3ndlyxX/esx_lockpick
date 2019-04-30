6c1ea4155743f3ce4cb61b976b16432e3e0e91c5
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-04-30 18:22:06 -0400

    Lockpick Percentage of success

--- config.lua
+++ config.lua
@@ -1,8 +1,9 @@
 Config						= {}
 Config.InfiniteLocks		= false  -- Should one lockpick last forever?
-Config.LockTime			    = 45     -- In seconds, how long should lockpicking take?
-Config.AlarmTime            = 30
+Config.LockTime			    = 5     -- In seconds, how long should lockpicking take?
+Config.AlarmTime            = 30     -- Second to have the alarm activated once vehicle is lockpicked
 Config.IgnoreAbort			= true   -- Remove lockpick from inventory even if user aborts lockpicking?
-Config.AllowMecano			= true  -- Allow mechanics to use this lockpick?
-Config.NPCVehiclesLocked    = true
+Config.AllowMecano			= true   -- Allow mechanics to use this lockpick?
+Config.NPCVehiclesLocked    = true   -- Lock all npc vehicles
 Config.Locale				= 'en'
+Config.percentage           = 50	 -- Percentage chance of success. Higher % mean better luck to get it unlocked.
