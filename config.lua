69750ee424cb703d2cef60dce973a25d5cf7dbf3
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-04-30 11:59:33 -0400

    Small Update
    Added config to lock npc vehicle.
    Alarms activate when vehicle has been lockpicked. You can choose the time duration.

--- config.lua
+++ config.lua
@@ -1,7 +1,8 @@
 Config						= {}
 Config.InfiniteLocks		= false  -- Should one lockpick last forever?
 Config.LockTime			    = 45     -- In seconds, how long should lockpicking take?
+Config.AlarmTime            = 30
 Config.IgnoreAbort			= true   -- Remove lockpick from inventory even if user aborts lockpicking?
 Config.AllowMecano			= true  -- Allow mechanics to use this lockpick?
-
+Config.NPCVehiclesLocked    = true
 Config.Locale				= 'en'
