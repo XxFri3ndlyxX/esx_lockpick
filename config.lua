2fe673c84dd3f6d870dad8d19b2903024dfe126c
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-05-01 19:02:29 -0400

    alert cops on failed attempts with %

--- config.lua
+++ config.lua
@@ -1,9 +1,11 @@
 Config						= {}
 Config.InfiniteLocks		= false  -- Should one lockpick last forever?
-Config.LockTime			    = 5     -- In seconds, how long should lockpicking take?
+Config.LockTime			    = 5      -- In seconds, how long should lockpicking take?
 Config.AlarmTime            = 30     -- Second to have the alarm activated once vehicle is lockpicked
 Config.IgnoreAbort			= true   -- Remove lockpick from inventory even if user aborts lockpicking?
 Config.AllowMecano			= true   -- Allow mechanics to use this lockpick?
 Config.NPCVehiclesLocked    = true   -- Lock all npc vehicles
-Config.Locale				= 'en'
-Config.percentage           = 50	 -- Percentage chance of success. Higher % mean better luck to get it unlocked.
+Config.Locale				= 'en'   -- Change the language. Currently only en  but will add fr soon.
+Config.percentage           = 50	 -- Percentage chance
+Config.CallCops             = true   -- Set to true if you want cops to be alerted when lockpicking has failed
+Config.CallCopsPercent      = 2      -- (min1) if 1 then cops will be called every time=100%, 2=50%, 3=33%, 4=25%, 5=20%.
