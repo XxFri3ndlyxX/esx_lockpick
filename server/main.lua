2996cd04f6cc197e5723e352bf55c412c6b74534
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-05-01 19:01:52 -0400

    Alert cops if failed with on %

--- server/main.lua
+++ server/main.lua
@@ -25,3 +25,25 @@ AddEventHandler('esx_lockpick:removeKit', function()
 		TriggerClientEvent('esx:showNotification', _source, _U('used_kit'))
 	end
 end)
+
+RegisterServerEvent('esx_lockpick:Notify')
+AddEventHandler('esx_lockpick:Notify', function()
+    TriggerClientEvent("esx_lockpick:Enable", source)
+end)
+
+
+RegisterServerEvent('esx_lockpick:InProgress')
+AddEventHandler('esx_lockpick:InProgress', function(street1, street2, sex)
+    TriggerClientEvent("esx_lockpick:outlawNotify", -1, "~r~Someone is lockpicking a vehicle")
+end)
+
+
+RegisterServerEvent('esx_lockpick:InProgressS1')
+AddEventHandler('esx_lockpick:InProgressS1', function(street1, sex)
+    TriggerClientEvent("esx_lockpick:outlawNotify", -1, "~r~Someone is lockpicking a vehicle")
+end)
+
+RegisterServerEvent('esx_lockpick:InProgressPos')
+AddEventHandler('esx_lockpick:InProgressPos', function(gx, gy, gz)
+    TriggerClientEvent('esx_lockpick:location', -1, gx, gy, gz)
+end)
