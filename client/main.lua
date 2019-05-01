b77b6dfd1c943419dee2b529ed133fd873273cc1
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-05-01 19:01:27 -0400

    Alert cops if failed with on %

--- client/main.lua
+++ client/main.lua
@@ -13,12 +13,24 @@ local Keys = {
 ESX						= nil
 local CurrentAction		= nil
 local PlayerData		= {}
+local pedIsTryingToLockpickVehicle  = false
+local timer = 1 --in minutes - Set the time during the player is outlaw
+local showOutlaw = true --Set if show outlaw act on map
+local blipTime = 35 --in second
+local showcopsmisbehave = true --show notification when cops steal too
+local timing = timer * 60000 --Don't touche it
 
 Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
 	end
+	
+	while ESX.GetPlayerData().job == nil do
+		Citizen.Wait(10)
+	end
+
+	PlayerData = ESX.GetPlayerData()
 end)
 
 RegisterNetEvent('esx:playerLoaded')
@@ -73,10 +85,17 @@ AddEventHandler('esx_lockpick:onUse', function()
 				end
 				CurrentAction = nil
 				TerminateThisThread()
+
 			elseif chance == false then
+				if Config.CallCops == true then
+					callcopstrue()
+			  elseif Config.CallCops == false then
+					callcopsfalse()
+			else
 				Citizen.Wait(Config.LockTime * 1000)
 				ClearPedTasksImmediately(playerPed)
 				ESX.ShowNotification(_U('picklock_failed'))
+			end
 			end
 			end)
 		end
@@ -101,6 +120,25 @@ AddEventHandler('esx_lockpick:onUse', function()
 		ESX.ShowNotification(_U('no_vehicle_nearby'))
 	end
 end)
+
+function callcopstrue()
+	local playerPed		= GetPlayerPed(-1)
+	local randomReport = math.random(1, Config.CallCopsPercent)
+	print(Config.CallCopsPercent)
+	if randomReport == Config.CallCopsPercent then
+		TriggerServerEvent('esx_lockpick:Notify')
+	Citizen.Wait(Config.LockTime * 1000)
+	ClearPedTasksImmediately(playerPed)
+	ESX.ShowNotification(_U('picklock_failed'))
+	end
+end
+
+function callcopsfalse()
+	local playerPed		= GetPlayerPed(-1)
+	Citizen.Wait(Config.LockTime * 1000)
+	ClearPedTasksImmediately(playerPed)
+	ESX.ShowNotification(_U('picklock_failed'))
+end
 
 function lockpickchance()
 	local nb = math.random(1, Config.percentage)
@@ -133,4 +171,116 @@ Citizen.CreateThread(function()
 			end
 		end
 	end
+end)
+
+--Only if Config.CallCops = true
+GetPlayerName()
+RegisterNetEvent('esx_lockpick:outlawNotify')
+AddEventHandler('esx_lockpick:outlawNotify', function(alert)
+		if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+						Notify2(alert)
+        end
+end)
+
+function Notify(text)
+    SetNotificationTextEntry('STRING')
+    AddTextComponentString(text)
+    DrawNotification(false, false)
+end
+
+function Notify2(msg)
+
+  local mugshot, mugshotStr = ESX.Game.GetPedMugshot(GetPlayerPed(-1))
+
+  ESX.ShowAdvancedNotification(_U('911'), _U('911Lockpick'), _U('call'), mugshotStr, 1)
+
+  UnregisterPedheadshot(mugshot)
+
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(100)
+        if NetworkIsSessionStarted() then
+            DecorRegister("IsOutlaw",  3)
+            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 1)
+            return
+        end
+    end
+end)
+
+Citizen.CreateThread( function()
+    while true do
+        Wait(100)
+        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
+        local street1 = GetStreetNameFromHashKey(s1)
+        local street2 = GetStreetNameFromHashKey(s2)
+        if pedIsTryingToLockpickVehicle then
+            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
+			if PlayerData.job ~= nil and PlayerData.job.name == 'police' and showcopsmisbehave == false then
+			elseif PlayerData.job ~= nil and PlayerData.job.name == 'police' and showcopsmisbehave then
+				ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+					local sex = nil
+					if skin.sex == 0 then
+						sex = "male" --male/change it to your language
+					else
+						sex = "female" --female/change it to your language
+					end
+					TriggerServerEvent('esx_lockpick:InProgressPos', plyPos.x, plyPos.y, plyPos.z)
+					if s2 == 0 then
+						TriggerServerEvent('esx_lockpick:InProgressS1', street1, sex)
+					elseif s2 ~= 0 then
+						TriggerServerEvent('esx_lockpick:InProgress', street1, street2, sex)
+					end
+				end)
+				Wait(3000)
+				pedIsTryingToLockpickVehicle = false
+			else
+				ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+					local sex = nil
+					if skin.sex == 0 then
+						sex = "male"
+					else
+						sex = "female"
+					end
+					TriggerServerEvent('esx_lockpick:InProgressPos', plyPos.x, plyPos.y, plyPos.z)
+					if s2 == 0 then
+						TriggerServerEvent('esx_lockpick:InProgressS1', street1, sex)
+					elseif s2 ~= 0 then
+						TriggerServerEvent('esx_lockpick:InProgress', street1, street2, sex)
+					end
+				end)
+				Wait(3000)
+				pedIsTryingToLockpickVehicle = false
+			end
+        end
+    end
+end)
+
+RegisterNetEvent('esx_lockpick:location')
+AddEventHandler('esx_lockpick:location', function(tx, ty, tz)
+	if PlayerData.job.name == 'police' then
+		local transT = 250
+		local Blip = AddBlipForCoord(tx, ty, tz)
+		SetBlipSprite(Blip,  10)
+		SetBlipColour(Blip,  1)
+		SetBlipAlpha(Blip,  transT)
+		SetBlipAsShortRange(Blip,  false)
+		while transT ~= 0 do
+			Wait(blipTime * 4)
+			transT = transT - 1
+			SetBlipAlpha(Blip,  transT)
+			if transT == 0 then
+				SetBlipSprite(Blip,  2)
+				return
+			end
+		end
+	end
+end)
+
+
+RegisterNetEvent('esx_lockpick:Enable')
+AddEventHandler('esx_lockpick:Enable', function()
+	pedIsTryingToLockpickVehicle = true
 end)
