59ce981f145cacb994639412bdd54bef97cddcf5
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-05-06 12:08:59 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,6 +7,6 @@ Config.IgnoreAbort			= true   -- Remove lockpick from inventory even if user abo
 Config.AllowMecano			= true   -- Allow mechanics to use this lockpick?
 Config.NPCVehiclesLocked    = true   -- Locks all vehicles (MUST HAVE SOME SORT OF LOCKSYSTEM FOR OWNED CAR) Will be adding a check for owned vehicle in the future. 
 Config.Locale				= 'en'   -- Change the language. Currently only en  but will add fr soon.
-Config.percentage           = 2	     -- (min1) if 1 then cops will be called every time=100%, 2=50%, 3=33%, 4=25%, 5=20%.
+Config.percentage           = 2	     -- (min1) 1=100%, 2=50%, 3=33%, 4=25%, 5=20%.
 Config.CallCops             = true   -- Set to true if you want cops to be alerted when lockpicking a vehicle no matter what the outcome is.
 Config.CallCopsPercent      = 1      -- (min1) if 1 then cops will be called every time=100%, 2=50%, 3=33%, 4=25%, 5=20%.
