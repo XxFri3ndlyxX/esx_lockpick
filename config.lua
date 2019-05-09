62a3ae5f82ebba9857cf491cf3487dc1091529a5
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-05-09 04:42:06 -0400

    Jammed Handbrake

--- config.lua
+++ config.lua
@@ -1,12 +1,13 @@
 Config						= {}
 Config.InfiniteLocks		= false  -- Should one lockpick last forever?
-Config.NotificationLockTime = 60     -- Must be the same as Config.Locktime
-Config.LockTime			    = 60     -- In seconds, how long should lockpicking take?
+Config.NotificationLockTime = 30     -- Must be the same as Config.Locktime
+Config.LockTime			    = 30     -- In seconds, how long should lockpicking take?
 Config.AlarmTime            = 30     -- Second to have the alarm activated once vehicle is lockpicked
+Config.JammedHandbrakeTime  = 10     -- Second to have the handbrake jammed
 Config.IgnoreAbort			= true   -- Remove lockpick from inventory even if user aborts lockpicking?
 Config.AllowMecano			= true   -- Allow mechanics to use this lockpick?
 Config.NPCVehiclesLocked    = true   -- Locks all vehicles (MUST HAVE SOME SORT OF LOCKSYSTEM FOR OWNED CAR) Will be adding a check for owned vehicle in the future. 
 Config.Locale				= 'en'   -- Change the language. Currently only en  but will add fr soon.
-Config.percentage           = 2	     -- (min1) 1=100%, 2=50%, 3=33%, 4=25%, 5=20%.
+Config.percentage           = 2	     -- (min1) if 1 then cops will be called every time=100%, 2=50%, 3=33%, 4=25%, 5=20%.
 Config.CallCops             = true   -- Set to true if you want cops to be alerted when lockpicking a vehicle no matter what the outcome is.
-Config.CallCopsPercent      = 1      -- (min1) if 1 then cops will be called every time=100%, 2=50%, 3=33%, 4=25%, 5=20%.
+Config.CallCopsPercent      = 2      -- (min1) if 1 then cops will be called every time=100%, 2=50%, 3=33%, 4=25%, 5=20%.
