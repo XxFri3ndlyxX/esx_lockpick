c33a4dc113bfd317f88bd9dc6cf424f90a4f792b
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-05-13 12:24:01 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -47,3 +47,58 @@ RegisterServerEvent('esx_lockpick:InProgressPos')
 AddEventHandler('esx_lockpick:InProgressPos', function(tx, ty, tz)
     TriggerClientEvent('esx_lockpick:location', -1, tx, ty, tz)
 end)
+
+local vehicles = {}
+
+function getVehData(plate, callback)
+    MySQL.Async.fetchAll("SELECT * FROM `owned_vehicles`", {},
+    function(result)
+        local foundIdentifier = nil
+        for i=1, #result, 1 do
+            local vehicleData = json.decode(result[i].vehicle)
+            if vehicleData.plate == plate then
+                foundIdentifier = result[i].owner
+                break
+            end
+        end
+        if foundIdentifier ~= nil then
+            MySQL.Async.fetchAll("SELECT * FROM `users` WHERE `identifier` = @identifier", {['@identifier'] = foundIdentifier},
+            function(result)
+                local ownerName = result[1].firstname .. " " .. result[1].lastname
+
+                local info = {
+                    plate = plate,
+                    owner = ownerName
+                }
+                callback(info)
+            end
+          )
+        else -- if identifier is nil then...
+          local info = {
+            plate = plate
+          }
+          callback(info)
+        end
+    end)
+  end
+
+RegisterNetEvent("esx_lockpick:setVehicleDoorsForEveryone")
+AddEventHandler("esx_lockpick:setVehicleDoorsForEveryone", function(veh, doors, plate)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local veh_model = veh[1]
+    local veh_doors = veh[2]
+    local veh_plate = veh[3]
+
+    if not vehicles[veh_plate] then
+        getVehData(veh_plate, function(veh_data)
+            if veh_data.plate ~= plate then
+                local players = GetPlayers()
+                for _,player in pairs(players) do
+                    TriggerClientEvent("esx_lockpick:setVehicleDoors", player, table.unpack(veh, doors))
+                end
+            end
+        end)
+        vehicles[veh_plate] = true
+    end
+end)
