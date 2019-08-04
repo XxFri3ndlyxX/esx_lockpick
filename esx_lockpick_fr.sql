5b7cdd3fd9d75e4181f62c3f785957402ad00e47
Dolu Tattoo <47056777+dolutattoo@users.noreply.github.com>
2019-08-04 10:30:30 +0200

    Create esx_lockpick_fr.sql for FRENCH language

---
+++ esx_lockpick_fr.sql
@@ -0,0 +1,11 @@
+USE `essentialmode`;
+
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('lockpick', 'Crochet', 3, 0, 1)
+;
+INSERT INTO `shops` (`store`, `item`, `price`) VALUES
+('TwentyFourSeven', 'Crochet', 10000),
+('LTDgasoline', 'Crochet', 10000),
+('RobsLiquor', 'Crochet', 10000)
+;
